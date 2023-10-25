bd6766a4e079582dcf3256b1f5330446146649ca
big alpaca🦙(zimeng) <zxzimeng@gmail.com>
2023-10-25 08:03:57 -0700

    created auto vex projects

---
+++ v1_autonomous/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       zimeng                                                       #
+# 	Created:      10/25/2023, 8:03:07 AM                                       #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
