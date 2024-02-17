d9062c6fb2c9a4ad8c79261fa25b1f95a5a83819
zimeng <zxzimeng@gmail.com>
2024-02-16 20:39:15 -0800

    half baked auton

--- driver2 copy/main2/src/main.py
+++ driver2 copy/main2/src/main.py
@@ -1,3 +1,29 @@
+#region VEXcode Generated Robot Configuration
+from vex import *
+import urandom
+
+# Brain should be defined by default
+
+# Robot configuration code
+inertial = Inertial(Ports.PORT17)
+
+
+# wait for rotation sensor to fully initialize
+wait(30, MSEC)
+
+
+def play_vexcode_sound(sound_name):
+    # Helper to make playing sounds from the V5 in VEXcode easier and
+    # keeps the code cleaner by making it clear what is happening.
+    print("VEXPlaySound:" + sound_name)
+    wait(5, MSEC)
+
+# add a small delay to make sure we don't print in the middle of the REPL header
+wait(200, MSEC)
+# clear the console to make sure we don't have the REPL in the console
+print("\033[2J")
+
+#endregion VEXcode Generated Robot Configuration
 import vex;
 from vex import *;
 
@@ -10,75 +36,74 @@ Right1 = Motor(Ports.PORT8, GearSetting.RATIO_18_1, False)
 Right2 = Motor(Ports.PORT9, GearSetting.RATIO_18_1, True)
 Right3 = Motor(Ports.PORT10, GearSetting.RATIO_18_1, True)
 intake = Motor(Ports.PORT4, GearSetting.RATIO_36_1, False)
-catapult = Motor(Ports.PORT5, GearSetting.RATIO_36_1, False)
+catapult = Motor(Ports.PORT6, GearSetting.RATIO_36_1, False)
 biden = DigitalOut(brain.three_wire_port.a)
 trump = DigitalOut(brain.three_wire_port.b)
 controller1 = Controller(PRIMARY)
-inertial = Inertial(Ports.PORT17)
 
 
-l = vex.MotorGroup(Left1, Left2, Left3)
-r = vex.MotorGroup(Right1, Right2, Right3)
-t = vex.MotorGroup(Left1, Left2, Left3, Right1, Right2, Right3)
+motor_group_l = MotorGroup(Left1, Left2, Left3)
+motor_group_r = MotorGroup(Right1, Right2, Right3)
+motor_group_t = MotorGroup(Left1, Left2, Left3, Right1, Right2, Right3)
 
 distance_per_degree = 3.14159*4/360
 wait(30, MSEC)
 
 def drive(distance):
     global distance_per_degree
-    global l
-    global r
-    #set velocity
-    l.set_velocity(60, PERCENT)
-    r.set_velocity(60, PERCENT)
+    global motor_group_l
+    global motor_group_r
+    global motor_group_t
 
     #calculate how much degress to spin wheel
-    spin_rotation = distance/distance_per_degree
+    spin_rotation = abs(distance/distance_per_degree)
+
 
     #reset encoders
+    if distance>0:
+        motor_group_t.set_velocity(30, PERCENT)
+        motor_group_t.spin_for(REVERSE, spin_rotation, DEGREES)
+    else:
+        motor_group_t.set_velocity(30, PERCENT)
+        motor_group_t.spin_for(FORWARD, spin_rotation, DEGREES)
 
-    l.spin_for(l_target_rotation, rotationUnits=RotationUnits.DEG)
-    r.spin_for(r_target_rotation, rotationUnits=RotationUnits.DEG)
-
-def spin_in_place(relative_heading):
-    #reset encoders
-    l.set_rotation(0)
-    r.set_rotation(0)
-    #set velocity
-    v_l = 60
-    v_r = -60
-    l.set_velocity(60)
-    r.set_velocity(60)
-    #set brake type
-    l.set_stopping(brake)
-    r.set_stopping(brake)
-    #reset heading
-    inertial.reset_heading(0)
-    inertial.reset_rotation(0)
-
-    initial_heading = inertial.angle()
-    target_heading = initial_heading+relative_heading
-
-    if relative_heading < 0:
-        v_l*=-1
-        v_r*=-1
     
-    while inertial.yaw() < target_heading:
-        l.spin()
-        r.spin()
-    
-    l.stop()
-    r.stop()
 
 
 def driver_control():
-    drive(5)
+    pass
+    
+
+
+
 
 def pre_autonomous():
-    inertial.start_calibrating()
+    inertial.calibrate()
 
 
 def autonomous():
-    pass
+    drive(16)
+    motor_group_l.set_velocity(30, PERCENT)
+    motor_group_l.spin_for(REVERSE, 150, DEGREES, wait=False)
+    motor_group_r.set_velocity(30, PERCENT)
+    motor_group_r.spin_for(FORWARD, 150, DEGREES,wait=True)
+    drive(-12)
+    trump.set(True)
+
+    start = brain.timer.time(SECONDS)
+    finish = start+1*60
+    catapult.set_max_torque(100, PERCENT)
+    intake.set_max_torque(100, PERCENT)
+
+    while brain.timer.time(SECONDS)<finish:
+        catapult.set_velocity(70, PERCENT)
+        intake.set_velocity(70*0.6, PERCENT)
+        catapult.spin(REVERSE)
+        intake.spin(FORWARD)
+    
+    intake.stop()
+    catapult.stop()
+
+
 
 competition = Competition(driver_control, autonomous)
